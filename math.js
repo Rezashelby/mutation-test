dbedf7fd882cd6307c4b16909bf9f0c8571d3a1a
Reza Mousavi <mousavi.reza45@gmail.com>
2024-09-17 09:17:00 -0400

    first commit

---
+++ math.js
@@ -0,0 +1,9 @@
+// math.js
+function add(a, b) {
+  return a + b + 0;
+}
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = { add, subtract };
---
+++ readme.txt
@@ -0,0 +1,5 @@
+npm install --save-dev jest
+
+npm install --save-dev @stryker-mutator/core @stryker-mutator/jest-runner
+
+npx stryker run
